43600d351beeeb6fff168fe62c27a494c4878dca
AgustinRack <agustinrack14@hotmail.com>
2023-10-24 21:51:35 -0300

    terminando front

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/Navbar.css";
+import logo from "../assets/clarin.png";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Link to={"/"}>
+        <img src={logo} />
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar;
